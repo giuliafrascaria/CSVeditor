c4ee2f93c2af59a85f4787f7fe8e9bac7fbe1537
dandi <deliterjob@gmail.com>
2017-01-20 11:05:42 +0100

    fixes

--- csvcreator.py
+++ csvcreator.py
@@ -9,25 +9,35 @@ try:
     csvWriter = csv.writer(singlelineCSV)
     dataList = list(readerFile)
     j = 1
-    for i in range (2, len(dataList)):
+    for i in range(2, len(dataList)):
         row = dataList[i]
-        print i
-        if len(row) <= 3:
-            print str(j) + "j"
+        if len(row) <= 11:
+            print(str(j) + "j")
             prevrow = dataList[i - j]
             smallrow = dataList[i]
             prevrow.extend(smallrow)
-            pr
+            print("riga corta, metto nella riga precedente")
+            #pr
             #vado avanti di una riga iterando il ciclo for
-            j+=1 #ma mi segno che devo incollare tutto una riga piu indietro
+            j += 1  #ma mi segno che devo incollare tutto una riga piu indietro
         else:
             csvWriter.writerow(dataList[i - j])
             j = 1
-            print row
+            #print(row)
             #copio la riga nel csv corretto
-            print "row normale"
-        csvWriter.writerow(dataList[i])
+            #print("row normale")
+            csvWriter.writerow(dataList[i])
 finally:
     generalCSV.close()
 
+source = open('singleline.csv')
+result = open('result.csv', 'wt')
 
+try:
+    rdr = csv.reader(source)
+    wtr = csv.writer(result)
+    for r in rdr:
+        wtr.writerow((r[1], r[2], r[3], r[10], r[11]))
+finally:
+    source.close()
+    result.close()
